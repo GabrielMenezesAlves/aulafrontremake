af68bfa34564348c8eb8600a034cb4bd35a66261
Gabriel Alves <gabriel_cm15@hotmail.com>
2024-11-27 21:58:13 -0300

    funcoes

---
+++ func.js
@@ -0,0 +1,16 @@
+
+// entrada -> processamento -> saida
+
+
+var n1 = 4;
+var n2 = 6;
+var n3 = 2;
+
+function soma(n1, n2){
+
+    var resultado = n1 + n2 + n3;
+    return resultado;
+
+}
+
+soma(2,3);
